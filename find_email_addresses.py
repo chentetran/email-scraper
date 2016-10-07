eb584c080c771f8d2a9f3526923f4db3c4ee6e60
chentetran <vincenttran23@gmail.com>
2016-10-07 14:47:28 -0400

    Added domain checker and already visited checker

--- find_email_addresses.py
+++ find_email_addresses.py
@@ -20,20 +20,23 @@ regex = re.compile(r"[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{1,4}", re.IGNORECAS
 
 domain = sys.argv[1]					# Get domain from command line
 if 'http' not in domain:				# Add protocol to string if not found
-	domain = 'https://' + domain
+	initialUrl = 'https://' + domain
 
 urlQueue 	= deque()					# Queue to track urls to visit
 urlsVisited = set()						# Set to track urls already visited
 emails 		= set()
-urlQueue.append(domain)
+urlQueue.append(initialUrl)
 browser 	= mechanize.Browser()
 
 while len(urlQueue):
-	print "[*] Trying "
+	url = urlQueue.popleft()
+	if url in urlsVisited:				# Already visited, skip
+		continue
+	print "[*] Trying link: " + url
 
 	# 0. Get source code & parse
 	try:
-		page 		= browser.open(urlQueue[0])
+		page 		= browser.open(url)
 		source_code = page.read()
 		parsed_source = BeautifulSoup(source_code, "html.parser")
 
@@ -44,16 +47,16 @@ while len(urlQueue):
 		# Find links by searching for <href> inside of <a>
 		for a in parsed_source.find_all('a'):
 			link = a.get('href')
-			if 'http' not in link:
-				link = 'http://' + link
-			urlQueue.append(link)
+			if domain in link:			# Only accept links within given domain
+				if 'http' not in link:
+					link = 'http://' + link
+				urlQueue.append(link)
 
-		#  TODO: should check that its on the domain
 		#  TODO: how do I tell if the link is valid???
 	except:
 		pass
 
-	urlsVisited.add(urlQueue.popleft())
+	urlsVisited.add(url)
 
 if len(emails) == 0:
 	print "[-] No emails found"

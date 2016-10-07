a4b849d86f0120969afdd54305269437220802da
chentetran <vincenttran23@gmail.com>
2016-10-07 17:16:04 -0400

    modularized code (added functions)

--- find_email_addresses.py
+++ find_email_addresses.py
@@ -11,57 +11,84 @@ import mechanize
 from collections import deque
 from bs4 		 import BeautifulSoup
 
-if len(sys.argv) == 1:
-	print "Usage: python find_email_addresses.py <domain>"
-	exit()
+''' 
+Takes a string text to search for emails
+Returns a list of all emails found
+'''
+def grabEmails(text):
+	return re.findall(regex, text)
 
-# Regex taken from http://code.activestate.com/recipes/138889-extract-email-addresses-from-files/
-regex = re.compile(r"[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{1,4}", re.IGNORECASE)
+'''
+Takes a BeautifulSoup object (parsed html) and a string url domain
+Returns a list of links that are within the given domain
+'''
+def grabLinks(parsed_text, domain):
+	links = []
+	
+	# Find links by searching for <href> inside of <a>
+	for a in parsed_text.find_all('a'):
+		link = a.get('href')
 
-domain = sys.argv[1]					# Get domain from command line
+		if 'http' not in link:		# Account for relative links
+			link = domain + link
+		if domain in link:		# Only accept links within given domain
+			links.append(link)
 
-if 'http' not in domain:				# Add protocol to string if not found
-	domain = 'https://' + domain
-domain += '/'
+	return links
 
-urlQueue 	= deque()					# Queue to track urls to visit
-urlsVisited = set()						# Set to track urls already visited
-emails 		= set()
-urlQueue.append(domain)
-browser 	= mechanize.Browser()
+'''
+Takes a string domain and a compiled regex
+Returns a list of all emails found
+'''
+def scrapeEmails(domain, regex):
+	urlQueue 	= deque()					# Queue to track urls to visit
+	urlsVisited = set()						# Set to track urls already visited
+	emails 		= set()
+	urlQueue.append(domain)
+	browser 	= mechanize.Browser()
 
-while len(urlQueue):
-	url = urlQueue.popleft()
-	if url in urlsVisited:				# Already visited, skip
-		continue
+	while len(urlQueue):
+		url = urlQueue.popleft()
+		if url in urlsVisited:				# Already visited, skip
+			continue
 
-	# 0. Get source code & parse
-	try:
-		page 		= browser.open(url)
-		source_code = page.read()
-		parsed_source = BeautifulSoup(source_code, "html.parser")
+		try:
+			page 		= browser.open(url)
+			source_code = page.read()
+			parsed_source = BeautifulSoup(source_code, "html.parser")
 
-		# 1. Grab emails from page @ front of queue 
-		emails.update(re.findall(regex, parsed_source.get_text()))
+			emails.update(grabEmails(parsed_source.get_text())) # Grab emails
+			urlQueue.extend(grabLinks(parsed_source, domain))	# Grab urls and add to queue
+		
+		except:
+			pass
 
-		# 2. Grab any urls & append to queue
-		# Find links by searching for <href> inside of <a>
-		for a in parsed_source.find_all('a'):
-			link = a.get('href')
+		urlsVisited.add(url)
 
-			if 'http' not in link:		# Account for relative links
-				link = domain + link
-			if domain in link:		# Only accept links within given domain
-				urlQueue.append(link)	
-	except:
-		pass
+	return list(emails)
 
-	urlsVisited.add(url)
+if __name__ == "__main__":
+	if len(sys.argv) == 1:
+		print "Usage: python find_email_addresses.py <domain>"
+		exit()
 
-if len(emails) == 0:
-	print "[-] No emails found"
-else:
-	print "[+] Found the following emails:"
-	for email in list(emails):
-		print "	" + email
+	# Regex taken from 
+	# http://code.activestate.com/recipes/138889-extract-email-addresses-from-files/
+	regex = re.compile(r"[\w\-][\w\-\.]+@[\w\-][\w\-\.]+[a-zA-Z]{1,4}", re.IGNORECASE)
+
+	domain = sys.argv[1]					# Get domain from command line
+
+	if 'http' not in domain:				# Add protocol to string if not found
+		domain = 'https://' + domain
+	domain += '/'
+
+
+	emails = scrapeEmails(domain, regex)
+
+	if len(emails) == 0:
+		print "[-] No emails found"
+	else:
+		print "[+] Found the following emails:"
+		for email in emails:
+			print "	" + email
 
